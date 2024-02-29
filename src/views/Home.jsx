279916fc49d44b88be72d034315034fde2138d96
Kimmo Ahola <128542718+KimmoKAhola@users.noreply.github.com>
2024-02-29 12:36:42 +0100

    adding material to the homepage

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import Showcase from "./sections/Showcase.jsx";
 
 const Home = () => {
     return (
-        <div>Home</div>
+        <div>
+            Home. I contain showcase
+            brands
+            app features
+            how does it work
+            make your money transfer...
+            clients are loving our app
+            any questions? check the FAQ
+            subscribe to newsletter
+            footer
+            <Showcase/>
+        </div>
     )
 }
 export default Home
--- src/views/sections/Showcase.jsx
+++ src/views/sections/Showcase.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import Mobile from '../../images/showcase-mobiles.svg';
 
 const Showcase = () => {
     return (
-        <div>Showcase</div>
+        <div>
+            <p>Manage All Your Money in One App</p>
+            <p>We offer you a new generation of the mobile banking.</p>
+            <p>Save, spend & manage money in your pocket.</p>
+            
+            <button>Download app store</button>
+            <button>Download google play</button>
+            
+            <button>Discover more</button>
+
+            <img src={Mobile} alt="showcase"/>
+        </div>
     )
 }
 export default Showcase
