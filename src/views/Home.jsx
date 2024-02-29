6916fc56ff2c0463e4f34855f1b57f416f53cf99
Kimmo Ahola <128542718+KimmoKAhola@users.noreply.github.com>
2024-02-29 12:41:13 +0100

    added a component for brands

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,19 +1,19 @@
 import React from 'react'
 import Showcase from "./sections/Showcase.jsx";
+import Brands from "./sections/Brands.jsx";
 
 const Home = () => {
     return (
         <div>
-            Home. I contain showcase
-            brands
             app features
-            how does it work
-            make your money transfer...
-            clients are loving our app
-            any questions? check the FAQ
-            subscribe to newsletter
+            Slider
+            #Features
+            #Testimonials
+            FAQ
+            subscribe
             footer
             <Showcase/>
+            <Brands/>
         </div>
     )
 }
---
+++ src/views/sections/Brands.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Brand1 from '../../images/brands/brand_1.svg'
+import Brand2 from '../../images/brands/brand_2.svg'
+import Brand3 from '../../images/brands/brand_3.svg'
+import Brand4 from '../../images/brands/brand_4.svg'
+import Brand5 from '../../images/brands/brand_5.svg'
+import Brand6 from '../../images/brands/brand_6.svg'
+
+const Brands = () => {
+    return (
+        <div>
+            <img src={Brand1} alt="brand 1"/>
+            <img src={Brand2} alt="brand 2"/>
+            <img src={Brand3} alt="brand 3"/>
+            <img src={Brand4} alt="brand 4"/>
+            <img src={Brand5} alt="brand 5"/>
+            <img src={Brand6} alt="brand 6"/>
+        </div>
+    )
+}
+export default Brands
