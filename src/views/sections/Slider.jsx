96e3af1a852f73a289a810ec3d51dd662fe8acfc
Kimmo Ahola <128542718+KimmoKAhola@users.noreply.github.com>
2024-02-29 19:13:29 +0100

    added some simple styling for Slider.jsx

--- src/views/sections/Slider.jsx
+++ src/views/sections/Slider.jsx
@@ -2,13 +2,22 @@ import React from 'react'
 import How1 from '../../images/mobile-howdoesitwork_1.svg'
 import How2 from '../../images/mobile-howdoesitwork_2.svg'
 import How3 from '../../images/mobile-howdoesitwork_3.svg'
+
 const Slider = () => {
     return (
-        <div>
-            <h1>How does it work?</h1>
-            <img src={How1} alt="TODO"/>
-            <img src={How2} alt="TODO"/>
-            <img src={How3} alt="TODO"/>
+        <div id="slider-container">
+
+            <h2>How Does It Work?</h2>
+
+            <div id="pictures">
+                <img src={How1} alt="TODO"/>
+                <img src={How2} alt="TODO"/>
+                <img src={How3} alt="TODO"/>
+            </div>
+
+            <h3>Step 2. Latest transaction history</h3>
+            <p>Enim, et amet praesent pharetra. Mi non ante hendrerit amet sed. Arcu sociis tristique quisque hac in
+                consectetur condimentum. </p>
         </div>
     )
 }
