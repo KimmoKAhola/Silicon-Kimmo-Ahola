7db338c73d02d703128c6fd84e04d599aa5cc854
Kimmo Ahola <128542718+KimmoKAhola@users.noreply.github.com>
2024-03-04 13:56:09 +0100

    starting to style the Footer-Dark.jsx

--- src/views/sections/global/Footer-Dark.jsx
+++ src/views/sections/global/Footer-Dark.jsx
@@ -3,7 +3,7 @@ import Logo from '../../../images/logo/silicon-logo-light_theme.svg'
 
 const FooterDark = () => {
     return (
-        <div>
+        <div id="footer-dark">
             <div>
                 <img src={Logo} alt="TODO"/>
                 <p>Proin ipsum pharetra, senectus eget scelerisque varius pretium platea velit. Lacus, eget eu vitae
@@ -11,32 +11,40 @@ const FooterDark = () => {
                     dolor ullamcorper sodales ultrices eros. </p>
                 <div>
                     <h4>Subscribe to our newsletter</h4>
-                    <p>Your email</p>
-                    <button>Subscribe</button>
+                    <div id="footer-subscribe">
+                        <p>Your email</p>
+                        <button>Subscribe</button>
+                    </div>
                 </div>
             </div>
-            <div id="site-links">
-                <ul>
-                    <li>Home</li>
-                    <li>Services</li>
-                    <li>Our Clients</li>
-                    <li>Testimonials</li>
-                    <li>News</li>
-                </ul>
-            </div>
+            <div id="footer-links">
+                <div id="site-links">
+                    <ul>
+                        <li>Home</li>
+                        <li>Services</li>
+                        <li>Our Clients</li>
+                        <li>Testimonials</li>
+                        <li>News</li>
+                    </ul>
+                    <ul>
+                        <li>Terms & Conditions</li>
+                        <li>Privacy Policy</li>
+                    </ul>
+                </div>
 
-            <div>
-                <ul>
-                    <li>Facebook</li>
-                    <li>LinkedIn</li>
-                    <li>Twitter</li>
-                    <li>Instagram</li>
-                </ul>
-            </div>
+                <div>
+                    <ul>
+                        <li>Facebook</li>
+                        <li>LinkedIn</li>
+                        <li>Twitter</li>
+                        <li>Instagram</li>
+                    </ul>
+                </div>
 
-            <div>
-                <p>Contact Us</p>
-                <a href="#">example@gmail.com</a>
+                <div id="footer-contact">
+                    <p>Contact Us</p>
+                    <a href="#">example@gmail.com</a>
+                </div>
             </div>
 
         </div>
