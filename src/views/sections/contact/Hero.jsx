c63bb9c5d38ef1ca9147e898d507ebdd5c80c2f3
Kimmo Ahola <128542718+KimmoKAhola@users.noreply.github.com>
2024-03-04 14:44:39 +0100

    starting to style the contact form part of the Contact.jsx page

--- src/views/sections/contact/Hero.jsx
+++ src/views/sections/contact/Hero.jsx
@@ -1,26 +1,40 @@
 import React from 'react'
 import ArrowButton from '../../../images/contact/arrow-btn.svg'
+import AddGroup from '../../../images/contact/add-group.svg'
+import Envelope from '../../../images/contact/bx-envelope.svg'
 
 const Hero = () => {
     return (
         <div>
-            <div>
+            <div className="hero">
                 <div className="contact-info">
                     <h1>Contact Us</h1>
 
-                    <h2>Email us</h2>
-                    <p>Please feel free to drop us a line. We will respond as soon as possible</p>
-                    <a href="#">Leave a message</a>
-                    <button className="arrow-button"><img src={ArrowButton} alt="TODO"/></button>
+                    <div id="email-us"><h2>Email us</h2>
+                        <div className="section">
+                            <img src={Envelope} alt="TODO"/>
+                            <div>
+                                <p>Please feel free to drop us a line. We will respond as soon as possible</p>
+                                <a href="#">Leave a message</a>
+                                <button className="arrow-button"><img src={ArrowButton} alt="TODO"/></button>
+                            </div>
+                        </div>
+                    </div>
 
-                    <h2>Careers</h2>
-                    <p>Sit ac ipsum leo lorem magna nunc mattis maecenas non vestibulum.</p>
-                    <a href="#">Send an application</a>
-                    <button className="arrow-button"><img src={ArrowButton} alt="TODO"/></button>
+                    <div id="careers"><h2>Careers</h2>
+                        <div className="section">
+                            <img src={AddGroup} alt="TODO"/>
+                            <div>
+                                <p>Sit ac ipsum leo lorem magna nunc mattis maecenas non vestibulum.</p>
+                                <a href="#">Send an application</a>
+                                <button className="arrow-button"><img src={ArrowButton} alt="TODO"/></button>
+                            </div>
+                        </div>
+                    </div>
                 </div>
 
                 <div className="contact-form">
-                <h2>Get Online Consultation</h2>
+                    <h2>Get Online Consultation</h2>
                     <p>Full name</p>
                     <input type="text"/>
 
