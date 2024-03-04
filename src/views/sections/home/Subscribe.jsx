677e3703751d5a5757425d1c05fa630ce9516e1c
Kimmo Ahola <128542718+KimmoKAhola@users.noreply.github.com>
2024-03-04 17:00:42 +0100

    added Subscribe.jsx and some preliminary styling

--- src/views/sections/home/Subscribe.jsx
+++ src/views/sections/home/Subscribe.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 import Notification from '../../../images/subscribe/notification.svg'
 const Subscribe = () => {
     return (
-        <div>
-            <div>
+        <div className="subscriber">
+            <div id="sub-left">
                 <img src={Notification} alt="TODO"/>
-                <h3>Subscribe to our newsletter to stay informed about latest updates</h3>
+                <h4>Subscribe to our newsletter to stay informed about latest updates</h4>
             </div>
-            <div>
-                <p>Your email</p>
+            <div id="sub-right">
+                <input type="text"/>
                 <button>Subscribe</button>
             </div>
         </div>
