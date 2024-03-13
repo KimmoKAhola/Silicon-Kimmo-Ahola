700efe7f65fbcafd41cabdbe2525876d90345375
Kimmo Ahola <128542718+KimmoKAhola@users.noreply.github.com>
2024-03-13 13:06:16 +0100

    starting with the error page

--- src/views/Error.jsx
+++ src/views/Error.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import Navbar from "./sections/global/Navbar.jsx";
 import Content from './sections/error/Content.jsx'
+
+function ErrorFooter() {
+    return <p>2024. All rights reserved. Silicon template.</p>
+}
+
 const Error = () => {
     return (
-        <div id="content-bg">
+        <div className="wrapper">
             <Navbar/>
             <Content/>
+            <ErrorFooter/>
         </div>
     )
 }
